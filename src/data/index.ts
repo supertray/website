f2f299dbb5492ca1f58757ae64bbd9b459bcb7fd
Phil <phil.schaffarzyk@gmail.com>
2024-02-22 18:06:43 +0100

    feat: added messages row

---
+++ src/data/index.ts
@@ -0,0 +1,77 @@
+export const data = {
+  en: {
+    headline1: 'Working with documents is broken.',
+    headline2: 'We are fixing it in the cloud or <span class="text-orange-10">on your server.</span>',
+    slackMessages: [
+      [
+        {
+          displayName: 'Sebastian',
+          time: '09:15 AM',
+          avatar: '/avatar/sebastian.webp',
+          message:
+            'Where can I find the contract for our freelancer Tom? I have looked for it inside shared/freelancer/contracts folder on our nas but only can find the first version A1 😖',
+          style: {
+            rotate: '5deg',
+          },
+        },
+      ],
+      [
+        {
+          displayName: 'Monica',
+          time: '02:35 PM',
+          avatar: '/avatar/monica.webp',
+          message: 'Hi Sue, I cannot access this doc. Can you please share it with me?',
+          style: {
+            rotate: '-2deg',
+            margin: '20px 0 0 0',
+          },
+        },
+        {
+          displayName: 'Sue',
+          time: '04:12 PM',
+          avatar: '/avatar/sue.webp',
+          message: 'I have shared it with you. Can you access it now?',
+          style: {
+            rotate: '-4deg',
+            margin: '-3px 0 0 15px',
+          },
+        },
+      ],
+      [
+        {
+          displayName: 'Alex',
+          time: '11:44 AM',
+          avatar: '/avatar/alex.webp',
+          message:
+            'WTF… I found version B4-final inside customers/acme/projects/website-relaunch/freelancers…<br />Is this really the final version?',
+          style: {
+            rotate: '3deg',
+            margin: '7px 0 0 -40px',
+          },
+        },
+      ],
+      [
+        {
+          displayName: 'Susan',
+          time: '08:31 AM',
+          avatar: '/avatar/susan.webp',
+          message: 'Hey, can you please send me the invoice of an amazon order from april 2022. We cannot find it…',
+          style: {
+            rotate: '-2deg',
+            margin: '-5px 0 0 -10px',
+          },
+        },
+        {
+          displayName: 'Ben',
+          time: '01:56 PM',
+          avatar: '/avatar/ben.webp',
+          message: 'I have sent it to you via mail on 4th april 2022!',
+          style: {
+            rotate: '1deg',
+            margin: '3px 0 0 -25px',
+          },
+        },
+      ],
+    ],
+  },
+};
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,17 +1,17 @@
 import { createPlugin } from 'windy-radix-palette';
 
-const colors = createPlugin();
+const colors = createPlugin({
+  opacitySupport: true,
+});
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'media',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      colors: {
-        orange: colors.alias('orange'),
-        gray: colors.alias('sand'),
-      },
+      colors: {},
     },
   },
-  plugins: [colors],
+  plugins: [colors.plugin],
 };
