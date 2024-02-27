c6c477329b2d286eada41d4575bbfa908d5b68de
Phil <phil.schaffarzyk@gmail.com>
2024-02-27 20:28:32 +0100

    ci: create env file

--- src/api/supabase.ts
+++ src/api/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://pmqiepfqhncefftenwth.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBtcWllcGZxaG5jZWZmdGVud3RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwNjAwNzEsImV4cCI6MjAyNDYzNjA3MX0.AHC1LtyNsyjmKIgyf1wFha-UNBNkc4RwP9BQiorizBw';
+const supabaseKey = import.meta.env.PUBLIC_SUPABASE_KEY as string;
 export const supabase = createClient(supabaseUrl, supabaseKey);
